ac02ea2d87d1984a542edd6d988894d4abdf4aa6
didwndckd <didwndckd@gmail.com>
2026-08-09 01:20:14 +0900

    init

---
+++ TCA-Example/TCA_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_ExampleApp.swift
+//  TCA-Example
+//
+//  Created by yjc on 8/9/26.
+//
+
+import SwiftUI
+
+@main
+struct TCA_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCA-ExampleTests/TCA_ExampleTests.swift
@@ -0,0 +1,19 @@
+//
+//  TCA_ExampleTests.swift
+//  TCA-ExampleTests
+//
+//  Created by yjc on 8/9/26.
+//
+
+import Testing
+@testable import TCA_Example
+
+struct TCA_ExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
